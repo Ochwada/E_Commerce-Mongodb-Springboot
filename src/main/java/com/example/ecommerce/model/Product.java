00b149b5d44036b461df59279f895abe424f897e
ochwada <nakhulolinda@yahoo.com>
2025-07-09 11:09:26 +0200

    product JOPO

---
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -0,0 +1,41 @@
+package com.example.ecommerce.model;
+
+
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * *******************************************************
+ * Package: com.example.ecommerce.model
+ * File: Product.java
+ * Author: Ochwada
+ * Date: Wednesday, 09.Jul.2025, 10:58 AM
+ * Description: Represent a product in our eCommerce catalog.
+ * Objective:
+ * *******************************************************
+ */
+
+
+@Data // Lombok annotation to automatically generate getters, setters, toString(), equals(), and hashCode() methods.
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor // exclude id from all arg constructor
+@Document(collection = "products")
+public class Product {
+    /** Unique identifier for the product */
+    @Id
+    private  String id;
+
+    /** Name of the product */
+    private  String name;
+
+    /** Product's description*/
+    private String description;
+
+    /** price of the product*/
+    private Double price;
+
+    /** The category the product belongs to*/
+    private  String category;
+}
